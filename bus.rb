b9bc238c3936b75596390a23bb29758fd77c2e89
Stephen Rooney <s.f.rooney@gmail.com>
2018-05-29 14:31:36 +0100

    Part B added

--- bus.rb
+++ bus.rb
@@ -6,13 +6,30 @@ attr_reader(:route_number, :destination)
   def initialize(route_number, destination)
     @route_number = route_number
     @destination = destination
+    @passengers = []
   end
 
  def drive
    return "brum brum"
  end
 
+ def number_of_passengers
+   @passengers.length()
+ end
 
+ def pick_up(passenger)
+   @passengers << passenger
+ end
+
+ def drop_off(passenger)
+
+   @passengers.delete(passenger) if @passengers.include?(passenger)
+ end
+
+
+ def empty
+   @passengers.clear()
+ end
 
 
 
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -1,11 +1,14 @@
 require ("minitest/autorun")
 require ("minitest/rg")
 require_relative("../bus.rb")
+require_relative("../person.rb")
 
 class BusTest < MiniTest::Test
 
   def setup
     @bus = Bus.new(22, "Haymarket")
+    @passenger1 = Person.new("chris", 29)
+    @passenger2 = Person.new("steve", 36)
   end
 
 
@@ -20,5 +23,33 @@ class BusTest < MiniTest::Test
   def test_bus_can_drive
     assert_equal("brum brum", @bus.drive())
   end
+
+  def test_bus_passenger_number
+   assert_equal(0, @bus.number_of_passengers())
+  end
+
+
+  def test_bus_picks_up
+    @bus.pick_up(@passenger1)
+    assert_equal(1 , @bus.number_of_passengers())
+
+  end
+
+  def test_bus_drops_off
+    @bus.pick_up(@passenger1)
+    @bus.drop_off(@passenger1)
+    assert_equal(0, @bus.number_of_passengers())
+  end
+
+  def test_bus_can_empty
+    @bus.pick_up(@passenger1)
+    @bus.pick_up(@passenger2)
+    @bus.empty()
+    assert_equal(0, @bus.number_of_passengers())
+  end
+
+
+
+
 
 end
