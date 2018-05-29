31d5b1aa62d7c97a47bfcd4b0af1c1f552da55e4
Stephen Rooney <s.f.rooney@gmail.com>
2018-05-29 13:57:16 +0100

    bus class tested

---
+++ bus.rb
@@ -0,0 +1,19 @@
+class Bus
+
+attr_reader(:route_number, :destination)
+
+
+  def initialize(route_number, destination)
+    @route_number = route_number
+    @destination = destination
+  end
+
+ def drive
+   return "brum brum"
+ end
+
+
+
+
+
+end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -4,6 +4,21 @@ require_relative("../bus.rb")
 
 class BusTest < MiniTest::Test
 
+  def setup
+    @bus = Bus.new(22, "Haymarket")
+  end
 
+
+  def test_bus_has_route_number
+    assert_equal( 22, @bus.route_number())
+  end
+
+  def test_bus_has_destination
+    assert_equal( "Haymarket", @bus.destination())
+  end
+
+  def test_bus_can_drive
+    assert_equal("brum brum", @bus.drive())
+  end
 
 end
